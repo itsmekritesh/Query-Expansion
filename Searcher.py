3b0180134399965f80001666297256dc4806db9b
Pcnitp <44447862+Pcnitp@users.noreply.github.com>
2018-12-21 01:24:08 +0530

    Add files via upload

--- Searcher.py
+++ Searcher.py
@@ -8,16 +8,19 @@ def start(keyword):
     res = requests.get('https://google.com/search?q='+keyword)
     soup = bs4.BeautifulSoup(res.text,'lxml')
     links = soup.select('.r a')
-    tab_counts = min(10, len(links))
+    tab_counts = min(11, len(links))
     
     for i in range(tab_counts):     #opening top 10 links
         #webbrowser.open('https://google.com' + links[i].get('href'))
+        if i==0:
+            continue
         link = 'https://google.com' + links[i].get('href')
         page = requests.get(link)
         soup = BeautifulSoup(page.content,'html.parser')
         
         #writing content of each page to a text file
-        with open("C:\\Users\\DELL\\Desktop\\Project\\New data\\paragraph.txt", 'a') as filehandle:
+        name="paragraph"+str(i)+".txt"
+        with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph\\"+name, 'w') as filehandle:
             for x in soup.find_all('header'):
                 filehandle.write(x.get_text()+"\n")
             for x in soup.find_all('p'):
@@ -39,26 +42,38 @@ from nltk.tag.stanford import StanfordPOSTagger
 import nltk
 import os
 
+
 java_path = "C:\\Program Files\\Java\\jre1.8.0_181\\bin\\java.exe"
 os.environ['JAVAHOME'] = java_path
-nltk.internals.config_java("C:/Program Files/Java/jre1.8.0_191/bin/java.exe")
+nltk.internals.config_java("C:/Program Files/Java/jre1.8.0_181/bin/java.exe")
 myTagger = StanfordPOSTagger('C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger','C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\stanford-postagger.jar')
-#myTagger.tag('What is the airspeed of an unladen swallow ?'.split())
+myTagger.tag('What is the airspeed of an unladen swallow ?'.split())
  
+nltk.internals.config_java(options='-xmx2G')
+
 def tagger():  
     
-    with open("C:\\Users\\DELL\\Desktop\\Project\\New data\\paragraph.txt", 'r') as file:
-        while(True):
-            str=file.readline()
-            if str=="":
-                break
-            list_tagged = myTagger.tag(str.split())
-            #print(list_tagged)
-            s=""
-            for obj in list_tagged:
-                s += obj[0] + "_" + obj[1] + " "
-            with open("C:\\Users\\DELL\\Desktop\\Project\\New data\\paragraph_tagged.txt", 'a') as filehandle:
-                filehandle.write(s+"\n")
+    for i in range(1,11):
+        name="paragraph"+str(i)+".txt"
+        print("1")
+        with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph\\"+name, 'r') as file:
+            while(True):
+                print("2")
+                read=file.readline()
+                print("3")
+                if read=="":
+                    break
+                print("4")
+                list_tagged = myTagger.tag(read.split())
+                #print(list_tagged)
+                print("5")
+                s=""
+                for obj in list_tagged:
+                    s = s + obj[0] + "_" + obj[1] + " "
+                print(s)
+                with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph tagged"+name, 'a') as filehandle:
+                    filehandle.write(s+"\n")
+                    print("7")
         
 tagger()
 
