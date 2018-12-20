458c69ab3d6bc6ebfad9108cb48cd63dc9ccdc6a
Pcnitp <44447862+Pcnitp@users.noreply.github.com>
2018-12-21 01:44:46 +0530

    Add files via upload

--- Searcher.py
+++ Searcher.py
@@ -47,7 +47,7 @@ java_path = "C:\\Program Files\\Java\\jre1.8.0_181\\bin\\java.exe"
 os.environ['JAVAHOME'] = java_path
 nltk.internals.config_java("C:/Program Files/Java/jre1.8.0_181/bin/java.exe")
 myTagger = StanfordPOSTagger('C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger','C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\stanford-postagger.jar')
-myTagger.tag('What is the airspeed of an unladen swallow ?'.split())
+#myTagger.tag('What is the airspeed of an unladen swallow ?'.split())
  
 nltk.internals.config_java(options='-xmx2G')
 
@@ -55,25 +55,18 @@ def tagger():
     
     for i in range(1,11):
         name="paragraph"+str(i)+".txt"
-        print("1")
         with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph\\"+name, 'r') as file:
             while(True):
-                print("2")
                 read=file.readline()
-                print("3")
                 if read=="":
                     break
-                print("4")
                 list_tagged = myTagger.tag(read.split())
                 #print(list_tagged)
-                print("5")
                 s=""
                 for obj in list_tagged:
                     s = s + obj[0] + "_" + obj[1] + " "
-                print(s)
-                with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph tagged"+name, 'a') as filehandle:
+                with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph tagged\\"+name, 'a') as filehandle:
                     filehandle.write(s+"\n")
-                    print("7")
         
 tagger()
 
