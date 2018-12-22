e3c08b3b58fa94347a218bcafca799d0db3182a9
Pcnitp <44447862+Pcnitp@users.noreply.github.com>
2018-12-23 00:03:00 +0530

    Add files via upload

--- Searcher.py
+++ Searcher.py
@@ -37,7 +37,7 @@ start('swine flu vaccine')
         #print(key)
         start(key)'''
     
- 
+''' 
 from nltk.tag.stanford import StanfordPOSTagger
 import nltk
 import os
@@ -49,7 +49,7 @@ nltk.internals.config_java("C:/Program Files/Java/jre1.8.0_181/bin/java.exe")
 myTagger = StanfordPOSTagger('C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger','C:\\Users\\DELL\\Desktop\\Project\\stanford-postagger-2018-10-16\\stanford-postagger.jar')
 #myTagger.tag('What is the airspeed of an unladen swallow ?'.split())
  
-nltk.internals.config_java(options='-xmx2G')
+#nltk.internals.config_java(options='-xmx2G')
 
 def tagger():  
     
@@ -57,16 +57,80 @@ def tagger():
         name="paragraph"+str(i)+".txt"
         with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph\\"+name, 'r') as file:
             while(True):
-                read=file.readline()
-                if read=="":
-                    break
-                list_tagged = myTagger.tag(read.split())
-                #print(list_tagged)
-                s=""
-                for obj in list_tagged:
-                    s = s + obj[0] + "_" + obj[1] + " "
-                with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph tagged\\"+name, 'a') as filehandle:
-                    filehandle.write(s+"\n")
-        
+                for j in range(10):
+                    read=file.readline()
+                    if read=="":
+                        break
+                    list_tagged = myTagger.tag(read.split())
+                    #print(list_tagged)
+                    s=""
+                    for obj in list_tagged:
+                        s = s + obj[0] + "_" + obj[1] + " "
+                    with open("C:\\Users\\DELL\\Desktop\\Project\\paragraph tagged\\"+name, 'a') as filehandle:
+                        filehandle.write(s+"\n")
+ '''
+ 
+import nltk
+#nltk.download('averaged_perceptron_tagger')
+def tagger():  
+    name="paragraph"+str(i).txt"
+    file = open("C:\\Users\\DELL\\Desktop\\Project\\paragraph\\"+name,'r')
+    words=[]
+    query_tagged = []
+    while (True):
+        query=file.readline()
+        if query=="":
+            break
+        text = nltk.word_tokenize(query)   
+        list_tagged =nltk.pos_tag(text)
+        #print(list_tagged)
+        str=""
+        for obj in list_tagged:
+            str = str + obj[0] + "_" + obj[1] + " "
+            
+       query_tagged.append(str)
+
+       i=0
+       while i<len(list_tagged):
+           cur = list_tagged[i][1][0]
+           wr = []
+           while i<len(list_tagged) and list_tagged[i][1]=="CD":
+               wr.append(list_tagged[i][0])
+               i=i+1
+           if i>=len(list_tagged) or list_tagged[i][1][0] not in ('N','J','V'):
+               for j in range(len(wr)):
+                   strt = ""
+                   for k in range(j,len(wr)):
+                       strt = strt + wr[k]
+                       #print(strt)
+                       words.append(strt)
+                       strt = strt + " "
+                   i=i+1
+                   continue
+           else:
+               cur = list_tagged[i][1][0]
+           if cur not in ('N','J','V'):
+               i=i+1
+               continue
+           while i<len(list_tagged) and (cur == list_tagged[i][1][0] or list_tagged[i][1]=="CD"):
+               wr.append(list_tagged[i][0])
+               i=i+1
+            #print(wr)
+           for j in range(len(wr)):
+               strt = ""
+               for k in range(j,len(wr)):
+                   strt = strt + wr[k]
+                   #print(strt)
+                   words.append(strt)
+                   strt = strt + " "
+           
+    with open('C:\\Users\\DELL\\Desktop\\Project\\New data\\test_extended.txt', 'w') as filehandle:
+        for i in words:
+            filehandle.write('%s\n' % i)
+
+    with open('C:\\Users\\DELL\\Desktop\\Project\\New data\\test_tagged.txt', 'w') as filehandle:
+        for i in query_tagged:
+            filehandle.write('%s\n' % i)
+       
 tagger()
 
